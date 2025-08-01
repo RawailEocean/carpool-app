b58fd366baa0b75e6209f80e2f70bd46c29fe0fa
RawailEocean <rawail.ahmed@eocean.net>
2025-08-01 11:31:40 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -234,7 +234,7 @@ export default function Home() {
             </div>
           </div>
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; 2024 Carpool. All rights reserved By Rawail Ahmed.</p>
+            <p>&copy; 2024 Carpool. All rights reserved By Rawail Ahmed!</p>
           </div>
         </div>
       </footer>
